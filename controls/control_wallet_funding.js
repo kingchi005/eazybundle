9104189fb992536e5984c6e7931dff9ce37be0b9
Kingchi <kingchi005@gmail.com>
2022-09-26 21:37:53 +0100

    utd

--- controls/control_wallet_funding.js
+++ controls/control_wallet_funding.js
@@ -20,7 +20,7 @@ const process_fund_wallet = async (email, amount) => {
   let New_balance = user.balance + amount;
   let trn = {user_name: user.user_name ,Type: 'Fund wallet' ,Description: 'Credit transaction' ,amount ,Phone: user.phone ,Previous_balance: user.balance ,New_balance }
   let res = await create_transaction(trn);
-	const notify = `Your wallet has been funded with the sum of NGN ${amount}, your new balance is NGN ${New_balance} |${Date.now()}`
+	const notify = `Your wallet has been succesfully funded with the sum of ₦ ${amount}, your new balance is ₦ ${New_balance} |${Date.now()}`
   if (res) {
   	User.updateMany({email : email}, {$push: {notifications: notify}})
   		.then(use => {})
