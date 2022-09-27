7ed01409f48f4b8998a6b43ec6ef4be39c9bfffe
Kingchi <kingchi005@gmail.com>
2022-09-27 15:43:36 +0100

    force-https

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const enforce = require('express-sslify');
 const mongoose = require('mongoose');
 require('dotenv').config();
+
+//Routes
 const apiRoutes = require('./routes/apiRoutes');
 const pageRoutes = require('./routes/pageRoutes');
 const authRoutes = require('./routes/authRoutes');
@@ -18,6 +21,7 @@ const fm = new FormatMoney({decimals: 2 });
 const app = express();
 
 //middleware
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
