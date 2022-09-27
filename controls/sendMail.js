35ee5faa97fbe994ccaa2d8c75c132d0ea7ac611
Kingchi <kingchi005@gmail.com>
2022-09-27 23:14:57 +0100

    sendMail

---
+++ controls/sendMail.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+// const sendMail = arg => {}
+
+module.exports = async (email, subject, message) => {
+	try {
+		const transporter = nodemailer.createTransport({
+			host: process.env.DOMAIN_NAME,
+			service: 'gmail',
+			// port: 587,
+			secure: true,
+			auth: {
+				// user: 'eazybundledata@gmail.com',
+				// pass: 'tpmtukkbtspvsauz'
+				user: process.env.MAIL_USER,
+				pass: process.env.MAIL_PASS
+			}
+		})
+
+		let info = await transporter.sendMail({
+			from: process.env.MAIL_USER,
+			to: email,
+			subject: subject,
+			text: message
+		})
+
+		return {message:'Email sent successfully', type: 'success', mail: info.response}
+	} catch(e) {
+		console.log(e);
+		return {error: {message:e.message, type: 'danger'}}
+	}
+}
